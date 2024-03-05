b29633124fbf80b9a8306a3010339783c94ed080
coderodent-calfee <robert.w.calfee@gmail.com>
2024-03-04 19:06:25 -0800

    better vis on nav

---
+++ src/components/noiseButton.js
@@ -0,0 +1,21 @@
+﻿/**
+ * This file is a signout component
+ *
+ */
+import React, {useState, useEffect, useReducer} from 'react';
+import {Link, Navigate} from 'react-router-dom';
+
+function NoiseButton({data, setData}) {
+    return (
+        <div>
+            <button className="pushable">
+              <span className="front">Revolver
+                <img src="/icons/revolver-gun-svgrepo-com.svg" alt="revolver-gun-svgrepo-com.svg" />
+              </span>
+            </button>
+        </div>
+    );
+}
+
+// Export out the React Component
+export default Test;
